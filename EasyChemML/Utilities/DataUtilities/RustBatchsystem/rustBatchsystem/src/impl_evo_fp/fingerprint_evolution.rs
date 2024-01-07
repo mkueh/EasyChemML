b67933a1032d4c5a5a894e51b038e8a1afdcbc23
Alexandra Ebberg <aebberg@uni-muenster.de>
2024-01-07 18:18:22 +0100

    Start implementing evolution steps

--- EasyChemML/Utilities/DataUtilities/RustBatchsystem/rustBatchsystem/src/impl_evo_fp/fingerprint_evolution.rs
+++ EasyChemML/Utilities/DataUtilities/RustBatchsystem/rustBatchsystem/src/impl_evo_fp/fingerprint_evolution.rs
@@ -1,7 +1,16 @@
+mod fingerprint_inheritance;
+
 use crate::impl_evo_fp::fitness_functions::single_dataset_fitness::FitnessFunctionConfig;
 use crate::impl_evo_fp::get_data::convert_data_to_mol;
-use crate::impl_evo_fp::population::generate_new_population;
+use crate::impl_evo_fp::population::{generate_new_population, Population};
 use ndarray::{Array, Array2, Ix1};
+use rdkit::ROMol;
+
+#[derive(Debug, PartialEq, thiserror::Error)]
+pub enum FingerprintEvolutionError {
+    #[error("could not convert smiles to romol (nullptr)")]
+    UnknownConversionError,
+}
 
 pub struct EvolutionConfig {
     pub evolution_steps: u8,
@@ -27,27 +36,61 @@ pub fn create_the_one_fingerprint(
     feature_data: &Array2<String>,
     regression_targets: &Option<Array<f64, Ix1>>,
     classification_targets: &Option<Array<usize, Ix1>>,
-) {
+) -> Result<(), std::io::Error> {
     // TODO: move upwards to only receive mol_data in this method
     let mol_data = convert_data_to_mol(feature_data);
     println!("One fingerprint to rule them all!!");
-    let mut first_population = generate_new_population(
-        evolution_config.population_size,
+    let (existing_population, found_steps) =
+        Population::get_highest_saved_population(&evolution_config.work_folder)?;
+    let mut population = if existing_population.is_none() {
+        generate_new_population(
+            evolution_config.population_size,
+            evolution_config.fp_size,
+            evolution_config.max_primitive_count,
+            evolution_config.max_bound_count,
+            evolution_config.bool_matching,
+            &mol_data,
+        )
+    } else {
+        existing_population.unwrap()
+    };
+    if found_steps as u8 >= evolution_config.evolution_steps {
+        panic!("Found higher population that indicated evolution steps")
+    }
+    if found_steps == 0 {
+        population.calculate_population_metrics(
+            &mol_data,
+            regression_targets.clone(),
+            classification_targets.clone(),
+            evolution_config.bool_matching,
+            fitness_function_config.clone(),
+        );
+        population.save_population(&evolution_config.work_folder, 0)?;
+    }
+
+    let evolved_population;
+    for i in 0..evolution_config.evolution_steps {
+        evolved_population = evolution_step(&evolution_config, &mol_data, &mut population);
+    }
+    Ok(())
+}
+
+fn evolution_step(evolution_config: &EvolutionConfig, feature_data: &Vec<ROMol>, start_population: &mut Population) {
+    let number_best =
+        evolution_config.proportion_best_fingerprints * evolution_config.population_size;
+    let number_new_fps =
+        evolution_config.proportion_new_population * evolution_config.population_size;
+    let number_kids = evolution_config.population_size - number_best - number_new_fps;
+
+    let new_population_members = generate_new_population(
+        number_new_fps,
         evolution_config.fp_size,
         evolution_config.max_primitive_count,
         evolution_config.max_bound_count,
         evolution_config.bool_matching,
-        &mol_data,
+        feature_data,
     );
-    first_population.calculate_population_metrics(
-        &mol_data,
-        regression_targets.clone(),
-        classification_targets.clone(),
-        evolution_config.bool_matching,
-        fitness_function_config.clone(),
-    );
-    let best_members = first_population.get_n_best_members(
-        (evolution_config.population_size as f32 * evolution_config.proportion_best_fingerprints)
-            as usize,
-    );
+    let best_members = start_population.get_n_best_members(number_best);
+
+    let kids =
 }
